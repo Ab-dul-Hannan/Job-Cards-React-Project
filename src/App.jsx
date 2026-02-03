f607b281764e57a610131d574e4f3acbb3a0e98d
Abdul Hannan <abdunknownsamurai@gmail.com>
2026-02-03 21:40:54 +0500

    Final Addition

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,19 @@
 import React from 'react'
 import Nav from './components/nav.jsx'
 import Card from './components/card.jsx'
-import AmazonPNG from './assets/amazonLogo.png'
+import { NavARR, CardContent } from './Data/data.jsx'
 
 const App = () => {
-  const NavARR = {
-    logo: 'Job Postings',
-    linkedIn: 'https://www.linkedin.com/in/abdul-hannan-929a762b8/',
-    gitHub: 'https://github.com/Ab-dul-Hannan',
-  }
-
-  const CardContent = {
-    imgSrc: AmazonPNG,
-    imgAlt: 'Amazon Logo',
-    company: 'Amazon',
-    time: '2 days ago',
-    jobTitle: 'Frontend Developer',
-    jobType: 'Full-time',
-    jobSitting: 'Remote',
-    price: '$12000',
-    location: 'San Francisco, CA',
-    applyBtn: 'Apply Now',
-  }
+  
 
   return (
     <>
       <Nav {...NavARR} />
 
       <div className="cardSection">
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
-        <Card {...CardContent}/>
+        {CardContent.map((job, index) => (
+          <Card key={index} {...job} />
+        ))}
       </div>
     </>
   )
